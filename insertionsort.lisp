da88c44d707368d9a07150106b09f8243b7bf61c
volosincu <bogdan.volosincu@riseup.net>
2016-08-07 10:06:23 +0000

    insertion sort - recursive implementation

--- insertionsort.lisp
+++ insertionsort.lisp
@@ -7,36 +7,59 @@
 ;; Copyright (c) 2016 Volosincu Mihai-Bogdan
 
 
+(defun insertion-sort (i &optional j elem ielem)
+  
+  (if (null j)
+      ;;recursive branch to iterate over j 
+      (insertion-sort i (+ i 1))
+
+      ;; insertion step
+      (if (and (>= j 0) (< i (- (length values) 1)))
+	  (let((v elem)
+	       (vi ielem))
+	    
+	    (if (null v)
+		(progn
+		  (setq vi (+ i 1))
+		  (setq v (nth vi values))))
+	    
+	    (if (< v (nth j values))
+		(let((tmp v))
+		  
+		  (setf (nth vi values) (nth j values))
+		  (setf (nth j values) v)
+		  (setq vi j)))
+	    ;; decrement j and send further the value of
+	    ;; current selection and new index after switch
+	    (insertion-sort i (decf j) v vi))
+
+	  ;; if insertion step ended for j increment i and reset j
+	  ;; recursive branch to iterate over i
+	  (if(< i (length values))
+	     (progn
+	       (insertion-sort (incf i))))
+	  )))
 
 
 
-(defun insertion-sort (lst i &optional j)
-  (if(car lst)
-     (if (< j (length values))
-	 (let ((el (nth (- 9 i) values))
-	       (k (- j 1)))
-	   (if (and (>= k 0) (< el (nth k values)))
-	       (let ((tmp (nth k values)))
-		 (setf (nth k values) el)
-		 (setf (nth (- 9 i) values) tmp)
-		 )
-	       )
-	   (progn
-	     (if(>= k 0)
-	      (print (list (- 9 i) k (nth (- 9 i) values) (nth k values) )) )
-	   
-	   (insertion-sort (cdr lst) i (incf j)))))
-     (if( < i (length values))
-	(insertion-sort (nthcdr (incf i) values) i 0))
-     ))
+(defparameter values '(4 0 7 8 6 2 3 9 5 1))
+(print ";;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;")
+(print values)
+(insertion-sort 0)
+(print values)
 
 
 
+(defparameter values '(4 8 7 0 1 2 3 9 5 6))
+(print ";;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;")
+(print values)
+(insertion-sort 0)
+(print values)
 
 
 
-
-
-
-(defparameter values '(4 8 7 0 1 2 3 9 5 6) )
-(insertion-sort values 0 0)
+(defparameter values '(1 6 2 3 8 7 0 9 5 4))
+(print ";;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;")
+(print values)
+(insertion-sort 0)
+(print values)
